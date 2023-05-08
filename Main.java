ae53316b4e7eb86a549868d5f3d38848b3c33925
Thalles Eduardo <eduardo.thalles905@gmail.com>
2023-05-08 16:16:38 -0300

    Listas_Encadeadas

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ListaDupla lista = new ListaDupla();
 
+        // Testes, faça como desejar ou copie abaixo.
         System.out.println("-------------------------------");
         lista.adicionar("Homer");
         lista.adicionar("Maggie");
@@ -43,7 +44,8 @@ public class Main {
         lista.adicionarNoComeco("Bart");
         System.out.println(lista.imprimir() + "\ntamanho da lista: " + lista.tamanho());
 
-        boolean[] check_0 = { lista.contem("Marge"), lista.contem("Homer"), lista.contem("Bart"), lista.contem("Maggie") };
+        boolean[] check_0 = { lista.contem("Marge"), lista.contem("Homer"), lista.contem("Bart"),
+                lista.contem("Maggie") };
         System.out.println(Arrays.toString(check_0) + "\ntamanho da lista: " + lista.tamanho());
 
         lista.esvaziaLista();
---
+++ README.md
@@ -0,0 +1,3 @@
+Neste exercício foi implementado as listas encadeadas em java, onde foi criado três classes, classe Celula onde ficará os construtores, 
+getters e setters, a classe ListaDupla onde está localizado os métodos que serão utilizados e a classe Main onde será executado esses
+métodos, tudo muito intuitivo sem requer necessidade de explicação.
